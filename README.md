952e2cdfb2925cc0a9ceb599528eaed30c0c1827
Torrey Meade <themeadeconcept@hotmail.com>
2021-03-06 19:05:12 -0500

    Full functionality was added, along with saving to local storage

---
+++ README.md
@@ -0,0 +1,9 @@
+This was a Seat Selector Widget, created to practice Javascript.
+
+Lessons learned:
+
+- DOM Manipulation
+- CSS
+- Nesting
+- adding classes dynamically
+- Local Storage (get, set)
--- script.js
+++ script.js
@@ -5,12 +5,27 @@ const count = document.getElementById("count");
 const total = document.getElementById("total");
 const movieSelect = document.getElementById("movie");
 
+populateUI();
+
 // + will turn value into a number
 let ticketPrice = +movieSelect.value;
+
+// Save selected movie index and price
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem("selectedMovieIndex", movieIndex);
+  localStorage.setItem("selectedMoviePrice", moviePrice);
+}
 
 // Update total and count
 function updateSelectedCount() {
   const selectedSeats = document.querySelectorAll(".row .seat.selected");
+
+  // copy selected seat into an array
+  // map through array
+  // return a new array of indexes
+  const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
+
+  localStorage.setItem("selectedSeats", JSON.stringify(seatsIndex));
 
   // Will store the length of the node list (class that is toggled)
   const selectedSeatsCount = selectedSeats.length;
@@ -19,14 +34,34 @@ function updateSelectedCount() {
   total.innerText = selectedSeatsCount * ticketPrice;
 }
 
+// Get data from local storage and populate UI
+function populateUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem("selectedSeats"));
+
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      // Checks if the seats are there (if it isn't there it is -1)
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add("selected");
+      }
+    });
+  }
+
+  const selectedMovieIndex = localStorage.getItem("selectedMovieIndex");
+
+  if (selectedMovieIndex !== null) {
+    movieSelect.selectedIndex = selectedMovieIndex;
+  }
+}
+
 // movie select event - listen for "change" which is the option
 movieSelect.addEventListener("change", (e) => {
   ticketPrice = +e.target.value;
+  setMovieData(e.target.selectedIndex, e.target.value);
   updateSelectedCount();
 });
 
 // seat click event
-
 // adds an event listener for anything in the container that is clicked
 container.addEventListener("click", (e) => {
   // checks if the element clicked has a class of seat but not a class of occupied
@@ -40,3 +75,6 @@ container.addEventListener("click", (e) => {
     updateSelectedCount();
   }
 });
+
+// Initial count and total set
+updateSelectedCount();
